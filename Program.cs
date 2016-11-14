abfe97ddfefeaa57f0824e502d80c42d2b5278cd
Adrian Godong <adrian.godong@gmail.com>
2016-11-13 23:09:22 -0800

    Wired CommandParser to Main

--- Program.cs
+++ Program.cs
@@ -4,9 +4,30 @@ namespace RobinhoodCli
 {
     public class Program
     {
+        private static CommandParser commandParser = new CommandParser();
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Robinhood CLI. Press Ctrl+C to exit.");
+            while (true)
+            {
+                Console.Write("> ");
+                var command = Console.ReadLine();
+                var order = commandParser.Parse(command);
+
+                if (order == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"ERROR: {commandParser.LastError}");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Sending order: {order.Type} {order.Symbol} - {order.Size} shares - ${order.LimitPrice} limit");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+            }
         }
     }
 }
